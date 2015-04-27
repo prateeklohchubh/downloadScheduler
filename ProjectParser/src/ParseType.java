58a67ca5540711759b4e84d402069cdbfcaae4f4
Vedanshi Kataria <vedanshi12117@iiitd.ac.in>
2015-04-28 00:17:38 +0530

    Parser functionality as in UML

---
+++ ProjectParser/src/ParseType.java
@@ -0,0 +1,7 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public interface ParseType {
+	public ParserType getType();
+	public ArrayList<String> getValueFromXML(File XML, String searchItem);
+}
---
+++ ProjectParser/src/XMLParser.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.util.ArrayList;
+
+
+public class XMLParser {
+	
+	ParserType series = ParserType.SERIES;
+	ParserType episode = ParserType.EPISODE;
+	ParseType dataParseType;
+
+	ArrayList<String> parserResult = new ArrayList<String>();
+
+	public void setParserasSeries()
+	{
+		dataParseType = new SeriesParser(series);
+	}
+	
+	public void setParserasEpisode()
+	{
+		dataParseType = new EpisodeParser(episode);
+	}
+	
+	public ArrayList<String> getAttributeValue(String attributeName)
+	{	
+		if(dataParseType.getType().equals(series))
+		{
+			File XML = new File("C:\\B.Tech\\6th Semester\\PP\\Project\\apiresult.xml");
+			dataParseType.getValueFromXML(XML, attributeName);
+			return parserResult;
+		}
+		else if(dataParseType.getType().equals(episode))
+		{
+			File XML = new File("C:\\B.Tech\\6th Semester\\PP\\Project\\apiresult.xml");
+			dataParseType.getValueFromXML(XML, attributeName);
+			return parserResult;
+		}
+		else
+		{
+			System.out.println("Set Parser first!");
+			return null;
+		}
+	}
+	
+}
