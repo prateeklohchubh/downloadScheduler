dce02ba0edafb77875248ad1b4c81e79ac822398
Vedanshi Kataria <vedanshi12117@iiitd.ac.in>
2015-04-28 00:15:49 +0530

    deleted parser

--- ProjectParser/src/ParseType.java
+++
@@ -1,7 +0,0 @@
-import java.io.File;
-import java.util.ArrayList;
-
-public interface ParseType {
-	public ParserType getType();
-	public ArrayList<String> getValueFromXML(File XML, String searchItem);
-}
